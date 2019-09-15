b6048719b4987309040d1b86431075691e7555b5
Max <mhandler1991@gmail.com>
2019-09-15 12:14:22 -0700

    + Dummy Data to be used

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+// *******************
+// DATA
+// *******************
+
+var friendsData = [
+    {
+        name: 'Person 1',
+        photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: 'Person 2',
+        photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+        scores: [
+            3,
+            1,
+            3,
+            4,
+            5,
+            5,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: 'Person 3',
+        photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: 'Person 4',
+        photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+        scores: [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        name: 'Person 5',
+        photo: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    }
+]
+
+// Export Data
+module.exports = friendsData;
