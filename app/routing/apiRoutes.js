4d98c66e57bc021ce418a23d2d96c9df37b1e5ba
Max <mhandler1991@gmail.com>
2019-09-15 12:00:51 -0700

    routing fixes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,28 @@
+//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+// *******************
+// Requirements
+// *******************
+
+var path = require('path');
+var friends = require('../data/friends');
+
+// *******************
+// EXPORT
+// *******************
+
+module.exports = function (app) {
+
+    // API GET
+    // Routing for API friends
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
+
+     // API POST
+     app.post('/app/friends', function (req, res) {
+
+     });
+
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,12 +8,16 @@ var path = require('path');
 // Routing
 // *******************
 
-// Routing to Survey HTML Page
-app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/survey.html'));
-});
+module.exports = function (app) {
 
-// Default Routing to Home Page
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
-})
+    // Routing to Survey HTML Page
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+
+    // Default Routing to Home Page
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+
+}
